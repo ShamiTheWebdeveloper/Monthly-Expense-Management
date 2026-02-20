d8a68e6add673917342c318c51930125623985a1
Ehtisham Asghar <ehtishamasghar227@gmail.com>
2026-02-20 21:54:27 +0500

    Deployment fixes

---
+++ config.php
@@ -0,0 +1,45 @@
+@@ -1,44 +0,0 @@
+<?php
+include 'db.php';
+$connection=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+if ($connection) {
+
+}else{
+    echo 'Error:'.$connection->connect_error;
+    exit();
+}
+$limit=52000;
+const ACTION_FILE = 'expense_action.php';
+define("GLOBAL_DATE", isset($_GET['date_search']) ? date('F Y', strtotime($_GET['date_search'])) : date('F Y'));
+date_default_timezone_set('Asia/Karachi');
+error_reporting(E_ALL);
+
+function get_categories($id=0): array
+{
+    global $connection;
+    $id_sql='';
+    if ($id!=0) $id_sql=" and id=".$id;
+    $query = "SELECT id,name FROM categories where status=1".$id_sql;
+    $result=$connection->query($query);
+    $output=[];
+    foreach($result as $row) $output[$row['id']]=$row['name'];
+    return $output;
+}
+
+function make_select($options,$name,$default='',$parameters='',$first_selected='Please select'): false|string
+{
+    ob_start();
+    ?>
+    <select name="<?= $name ?>" <?= $parameters ?>>
+        <?php if ($first_selected!=''): ?>
+            <option <?= $default==''?'selected':''; ?> ><?= $first_selected ?></option>
+        <?php endif; ?>
+        <?php foreach ($options as $key=>$option): ?>
+            <option value="<?= $key ?>" <?php if ($default!='') echo $default===$key?"selected":""; ?>><?= $option ?></option>
+        <?php endforeach; ?>
+    </select>
+    <?php
+    $output=ob_get_contents();
+    ob_end_clean();
+    return $output;
+}
